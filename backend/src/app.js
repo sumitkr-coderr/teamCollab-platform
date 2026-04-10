b09cd4f210ebf9b114698a4d095cf5423075777a
Sumit kumar <shumeet123@gmail.com>
2026-04-10 09:22:17 +0000

    fix more error in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,7 +23,7 @@ app.use(helmet({
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://your-app.vercel.app"
+    "https://your-frontend.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
   credentials: true,
