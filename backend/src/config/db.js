19821834a3c4056ced97947772691b380f3c7357
Sumit <shumeetjha@gmail.com>
2026-04-21 22:49:02 +0530

    fix railway variable

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,22 +1,24 @@
+// 
 import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
   {
-    host: process.env.DB_HOST,
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
     dialect: "mysql",
-    logging: false, // disable SQL logs (enable in dev if needed)
+    logging: false,
   }
 );
 
 export const connectDB = async () => {
   try {
     await sequelize.authenticate();
+    console.log("✅ MySQL connected");
   } catch (error) {
     console.error("❌ Unable to connect to MySQL:", error.message);
     process.exit(1);
   }
 };
-
